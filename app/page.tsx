c7002facd072b6055d1651e86ed0883bf9c318aa
Elliot Kang <kkang2097@gmail.com>
2025-05-03 17:57:16 -0700

    top-level ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import PageContainer from "./components/PageContainer";
 import ChatWidget from "./components/ChatWidget"; 
+import TopBar from "./components/TopBar";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <h1 className="text-2xl font-bold text-gray-800">Leetcode for System Design</h1>
+      <TopBar />
       <PageContainer/>
       <ChatWidget />
     </div>
